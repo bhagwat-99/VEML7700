3d46d2e0ec869b5087eb7068b76b95167422f7e1
bhagwat-99 <shindebhagavat030@gmail.com>
2022-02-24 18:47:35 +0530

    transfer receive msg

--- VEML7700.c
+++ VEML7700.c
@@ -11,27 +11,42 @@
 
 int main()
 {
-    struct i2c_msg msg[1];
+    struct i2c_msg msg[2];
     unsigned char num_byte = 2;
-    unsigned char num_msg = 1;
+    unsigned char num_msg = 2;
 
-    //data buffer
-    unsigned char buffer[2];
-    buffer[0]=0xf0;
-    buffer[1]=0xff;
+ 
+    //transfer data buffer
+    unsigned char transfer_buffer[2];
+    transfer_buffer[0]=0x00;
+    transfer_buffer[1]=0x00;
+
+    //receive data buffer
+    unsigned char receive_buffer[2];
+    receive_buffer[0]=0x00;
+    receive_buffer[1]=0x00;
+
 
     //transfer msg
     msg[0].addr = 0x00;
     msg[0].flags = 0; //flag 0 for write
     msg[0].len = num_byte;
-    msg[0].buf = buffer;
+    msg[0].buf = transfer_buffer;
+
+
+    // receive msg
+    msg[1].addr = 0x04;
+    msg[1].flags = I2C_M_RD;
+    msg[1].len = num_byte;
+    msg[1].buf = receive_buffer;
 
     struct i2c_rdwr_ioctl_data tranfer_data;
     tranfer_data.msgs = msg;
     tranfer_data.nmsgs = num_msg;
     
+    
 
-     // Open file descriptor to I2C bus
+    // Open file descriptor to I2C bus
     int file = open("/dev/apalis-i2c1",O_RDWR);
     if(file<0) {
     printf("Failed to open apalis-i2c1.");
